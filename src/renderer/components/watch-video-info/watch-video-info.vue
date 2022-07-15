68f8e920b44e93429be5732df6de5e2734518771
Alin <ai6@tutanota.com>
2022-07-15 13:02:43 +0100

    make channel open with middle click

--- src/renderer/components/watch-video-info/watch-video-info.vue
+++ src/renderer/components/watch-video-info/watch-video-info.vue
@@ -13,19 +13,29 @@
           class="profileRow"
         >
           <div>
-            <img
-              :src="channelThumbnail"
-              class="channelThumbnail"
-              @click="goToChannel"
+            <router-link
+             class="channelName"
+             :to="`/channel/${channelId}`" 
             >
+              <img
+                :src="channelThumbnail"
+                class="channelThumbnail"
+                @click="goToChannel"
+               >
+            </router-link>
           </div>
           <div>
-            <div
-              class="channelName"
-              @click="goToChannel"
-            >
-              {{ channelName }}
-            </div>
+            <router-link
+             style="text-decoration:none; color:#FFF;"
+             :to="`/channel/${channelId}`"
+             >
+              <div
+                class="channelName"
+                @click="goToChannel"
+              >
+                {{ channelName }}
+              </div>
+             </router-link>
             <ft-button
               v-if="!hideUnsubscribeButton"
               :label="subscribedText"
